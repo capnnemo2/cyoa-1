9094ae6c25b1ab05ac785b86986689b06d922a93
Ben Hernandez <benjammin234@gmail.com>
2020-08-06 10:15:11 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>Choose Your Own Adventure</header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <p>
+          Choose Your Own Adventure books were pretty exciting when I discovered
+          them in elementary school. They were a way to basically play a video
+          or computer game through a book. I though they were awesome. I wanted
+          to create one as a web app. Of course, this is right on the line of
+          creating an actual game. As such, as the project advances, it may
+          eventually blur the line. But to begin, a simple CYOA story!
+        </p>
+      </div>
+    );
+  }
+}
